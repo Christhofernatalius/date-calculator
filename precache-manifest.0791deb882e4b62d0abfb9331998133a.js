0e54dc3055846c4a2dda1f1e6b9084b43d1c8418
Christhofer Natalius <christhofernatalius@gmail.com>
2019-04-15 01:51:15 +0700

    deploy

---
+++ precache-manifest.0791deb882e4b62d0abfb9331998133a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/date-calculator/robots.txt"
+  },
+  {
+    "revision": "ce5813d527833d53e585",
+    "url": "/date-calculator/js/chunk-vendors.6e7994e1.js"
+  },
+  {
+    "revision": "851442af33c5ef6b54f8",
+    "url": "/date-calculator/js/app.0abe9336.js"
+  },
+  {
+    "revision": "29237e3192648608c6f911c09f2a24f2",
+    "url": "/date-calculator/index.html"
+  },
+  {
+    "revision": "0d1ebc5117bf8f719f8539971f55415a",
+    "url": "/date-calculator/img/logo.png"
+  },
+  {
+    "revision": "851442af33c5ef6b54f8",
+    "url": "/date-calculator/css/app.7bb6b072.css"
+  }
+];
